3b29eb883b9399f3f2fb1b5c9a26175f59c0a2b8
Dain Sundstrom <dain@iq80.com>
2022-09-21 14:34:54 -0700

    Add AggregationMaskCompiler

--- core/trino-main/src/main/java/io/trino/sql/planner/LocalExecutionPlanner.java
+++ core/trino-main/src/main/java/io/trino/sql/planner/LocalExecutionPlanner.java
@@ -3925,8 +3925,7 @@ public class LocalExecutionPlanner
                     argumentChannels,
                     maskChannel,
                     !aggregation.isDistinct() && aggregation.getOrderingScheme().isEmpty(),
-                    lambdaProviders,
-                    resolvedFunction.getFunctionNullability());
+                    lambdaProviders);
         }
 
         private List<Supplier<Object>> makeLambdaProviders(List<LambdaExpression> lambdaExpressions, List<Class<?>> lambdaInterfaces, List<FunctionType> functionTypes)
