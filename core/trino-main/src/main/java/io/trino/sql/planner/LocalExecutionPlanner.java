888e8f7e8c882c8de83d05698b5cdcdda6595346
Dain Sundstrom <dain@iq80.com>
2022-09-21 20:55:05 -0700

    Change Accumulator addInput to take AggregationMask

--- core/trino-main/src/main/java/io/trino/sql/planner/LocalExecutionPlanner.java
+++ core/trino-main/src/main/java/io/trino/sql/planner/LocalExecutionPlanner.java
@@ -3925,7 +3925,8 @@ public class LocalExecutionPlanner
                     argumentChannels,
                     maskChannel,
                     !aggregation.isDistinct() && aggregation.getOrderingScheme().isEmpty(),
-                    lambdaProviders);
+                    lambdaProviders,
+                    resolvedFunction.getFunctionNullability());
         }
 
         private List<Supplier<Object>> makeLambdaProviders(List<LambdaExpression> lambdaExpressions, List<Class<?>> lambdaInterfaces, List<FunctionType> functionTypes)
