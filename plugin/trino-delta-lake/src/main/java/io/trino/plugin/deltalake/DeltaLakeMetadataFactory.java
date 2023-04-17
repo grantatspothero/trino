da6dcc6395277958c7201942bd743599b953ff58
Piotr Findeisen <piotr.findeisen@gmail.com>
2023-04-17 22:18:20 +0200

    Remove Delta Metastore transaction log validation on createTable
    The caller is responsible for providing correct location and metastore
    doesn't need to verify that.

--- plugin/trino-delta-lake/src/main/java/io/trino/plugin/deltalake/DeltaLakeMetadataFactory.java
+++ plugin/trino-delta-lake/src/main/java/io/trino/plugin/deltalake/DeltaLakeMetadataFactory.java
@@ -114,7 +114,6 @@ public class DeltaLakeMetadataFactory
         AccessControlMetadata accessControlMetadata = accessControlMetadataFactory.create(cachingHiveMetastore);
         HiveMetastoreBackedDeltaLakeMetastore deltaLakeMetastore = new HiveMetastoreBackedDeltaLakeMetastore(
                 cachingHiveMetastore,
-                transactionLogAccess,
                 fileSystemFactory);
         FileBasedTableStatisticsProvider tableStatisticsProvider = new FileBasedTableStatisticsProvider(
                 typeManager,
