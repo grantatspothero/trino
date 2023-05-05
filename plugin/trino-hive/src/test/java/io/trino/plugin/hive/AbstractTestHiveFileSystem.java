4a9c0afcc8b0379eba8b23669f549c9063255813
Alex Jo <jo.alex2144@gmail.com>
2023-05-05 12:38:21 -0400

    Remove session property overrides for Hive staging location
    Users should not have the ability to write files into arbitary locations.

--- plugin/trino-hive/src/test/java/io/trino/plugin/hive/AbstractTestHiveFileSystem.java
+++ plugin/trino-hive/src/test/java/io/trino/plugin/hive/AbstractTestHiveFileSystem.java
@@ -199,7 +199,7 @@ public abstract class AbstractTestHiveFileSystem
                                 .build()),
                 getBasePath(),
                 hdfsEnvironment);
-        locationService = new HiveLocationService(hdfsEnvironment);
+        locationService = new HiveLocationService(hdfsEnvironment, config);
         JsonCodec<PartitionUpdate> partitionUpdateCodec = JsonCodec.jsonCodec(PartitionUpdate.class);
         metadataFactory = new HiveMetadataFactory(
                 LocationAccessControl.ALLOW_ALL,
