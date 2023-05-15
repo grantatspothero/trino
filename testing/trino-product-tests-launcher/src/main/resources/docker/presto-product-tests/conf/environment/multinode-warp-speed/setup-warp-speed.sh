078f3c7db605d2c6be87bfef319985d16118eb57
Kobi Luz <kobi@varada.io>
2023-05-15 09:33:42 +0300

    warm speed remove unused block device configuration for PT

--- testing/trino-product-tests-launcher/src/main/resources/docker/presto-product-tests/conf/environment/multinode-warp-speed/setup-warp-speed.sh
+++ testing/trino-product-tests-launcher/src/main/resources/docker/presto-product-tests/conf/environment/multinode-warp-speed/setup-warp-speed.sh
@@ -2,13 +2,8 @@
 
 set -exuo pipefail
 
-# create local device
-dd if=/dev/zero of=/opt/ssd0 bs=1M count=10240
-mknod /dev/fakessd0 b 7 200
-# deleting the device if exists, ignoring the error if it doesn't
-losetup -d /dev/fakessd0 || true
-losetup /dev/fakessd0 /opt/ssd0
-dd if=/dev/fakessd0 of=/tmp/page count=1 bs=8K
+# configure libaio 
 echo "8388608" > /proc/sys/fs/aio-max-nr
-#disk for native
+
+# disk for native
 mkdir -p /opt/data
