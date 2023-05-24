6ec09281fb638f3ce9c8dd879d7a47142704080a
Yuya Ebihara <ebyhry@gmail.com>
2023-05-24 11:59:48 +0900

    Add materialized view test for object store connector with Glue

---
+++ plugin/trino-galaxy-objectstore/src/test/java/io/trino/plugin/objectstore/TestObjectStoreIcebergGlueCatalogMaterializedView.java
@@ -0,0 +1,132 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.trino.plugin.objectstore;
+
+import com.amazonaws.services.glue.AWSGlueAsync;
+import com.amazonaws.services.glue.AWSGlueAsyncClientBuilder;
+import com.amazonaws.services.glue.model.BatchDeleteTableRequest;
+import com.amazonaws.services.glue.model.DeleteDatabaseRequest;
+import com.amazonaws.services.glue.model.GetTablesRequest;
+import com.amazonaws.services.glue.model.GetTablesResult;
+import com.amazonaws.services.glue.model.Table;
+import com.google.common.collect.ImmutableMap;
+import io.trino.hdfs.TrinoFileSystemCache;
+import io.trino.plugin.hive.aws.AwsApiCallStats;
+import io.trino.plugin.iceberg.BaseIcebergMaterializedViewTest;
+import io.trino.plugin.iceberg.IcebergPlugin;
+import io.trino.plugin.tpch.TpchPlugin;
+import io.trino.server.security.galaxy.GalaxyTestHelper;
+import io.trino.testing.DistributedQueryRunner;
+import io.trino.testing.GalaxyQueryRunner;
+import io.trino.testing.QueryRunner;
+import org.testng.annotations.AfterClass;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+import static com.google.common.collect.ImmutableSet.toImmutableSet;
+import static io.trino.plugin.hive.metastore.glue.AwsSdkUtil.getPaginatedResults;
+import static io.trino.plugin.objectstore.TableType.ICEBERG;
+import static io.trino.plugin.objectstore.TestingObjectStoreUtils.createObjectStoreProperties;
+import static io.trino.server.security.galaxy.GalaxyTestHelper.ACCOUNT_ADMIN;
+import static io.trino.testing.TestingNames.randomNameSuffix;
+import static java.lang.String.format;
+
+public class TestObjectStoreIcebergGlueCatalogMaterializedView
+        extends BaseIcebergMaterializedViewTest
+{
+    private static final String TEST_CATALOG = "iceberg";
+
+    private String schemaName;
+
+    @Override
+    protected QueryRunner createQueryRunner()
+            throws Exception
+    {
+        GalaxyTestHelper galaxyTestHelper = closeAfterClass(new GalaxyTestHelper());
+        galaxyTestHelper.initialize();
+
+        closeAfterClass(TrinoFileSystemCache.INSTANCE::closeAll);
+        TestingLocationSecurityServer locationSecurityServer = closeAfterClass(new TestingLocationSecurityServer((session, location) -> true));
+
+        schemaName = "test_objectstore_iceberg_glue_" + randomNameSuffix();
+
+        Map<String, String> properties = createObjectStoreProperties(
+                ICEBERG,
+                ImmutableMap.<String, String>builder()
+                        .putAll(locationSecurityServer.getClientConfig())
+                        .put("galaxy.catalog-id", "c-1234567890")
+                        .buildOrThrow(),
+                ImmutableMap.of(),
+                ImmutableMap.of(),
+                ImmutableMap.<String, String>builder()
+                        .put("HIVE__hive.metastore", "glue")
+                        .put("ICEBERG__iceberg.catalog.type", "glue")
+                        .put("ICEBERG__hive.metastore.glue.default-warehouse-dir", getSchemaDirectory())
+                        .put("DELTA__hive.metastore", "glue")
+                        .put("HUDI__hive.metastore", "glue")
+                        .buildOrThrow());
+        DistributedQueryRunner queryRunner = GalaxyQueryRunner.builder(TEST_CATALOG, schemaName)
+                .setAccountClient(galaxyTestHelper.getAccountClient())
+                .addPlugin(new IcebergPlugin())
+                .addPlugin(new ObjectStorePlugin())
+                .addCatalog(TEST_CATALOG, "galaxy_objectstore", properties)
+                .addPlugin(new TpchPlugin())
+                .addCatalog("tpch", "tpch", ImmutableMap.of())
+                .build();
+        queryRunner.execute("CREATE SCHEMA %s.%s".formatted(TEST_CATALOG, schemaName));
+        queryRunner.execute("GRANT SELECT ON tpch.\"*\".\"*\" TO ROLE %s WITH GRANT OPTION".formatted(ACCOUNT_ADMIN));
+        return queryRunner;
+    }
+
+    @Override
+    protected String getSchemaDirectory()
+    {
+        return format("s3://galaxy-trino-ci/%s", schemaName);
+    }
+
+    @Override
+    protected boolean isObjectStore()
+    {
+        return true;
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void cleanup()
+    {
+        cleanUpSchema(schemaName);
+        cleanUpSchema(storageSchemaName);
+    }
+
+    private static void cleanUpSchema(String schema)
+    {
+        AWSGlueAsync glueClient = AWSGlueAsyncClientBuilder.defaultClient();
+        Set<String> tableNames = getPaginatedResults(
+                glueClient::getTables,
+                new GetTablesRequest().withDatabaseName(schema),
+                GetTablesRequest::setNextToken,
+                GetTablesResult::getNextToken,
+                new AwsApiCallStats())
+                .map(GetTablesResult::getTableList)
+                .flatMap(Collection::stream)
+                .map(Table::getName)
+                .collect(toImmutableSet());
+        glueClient.batchDeleteTable(new BatchDeleteTableRequest()
+                .withDatabaseName(schema)
+                .withTablesToDelete(tableNames));
+        glueClient.deleteDatabase(new DeleteDatabaseRequest()
+                .withName(schema));
+    }
+}
