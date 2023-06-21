f8c379d3dd5d71632a585291ea3331e5e4772442
Vlad Lyutenko <vlad.lyutenko@starburstdata.com>
2023-06-21 21:43:26 +0200

    Refactor identify mapping to use in non jdbc connector
    And move identify mapping to plugin toolkit

--- plugin/trino-druid/src/main/java/io/trino/plugin/druid/DruidJdbcClientModule.java
+++ plugin/trino-druid/src/main/java/io/trino/plugin/druid/DruidJdbcClientModule.java
@@ -25,7 +25,6 @@ import io.trino.plugin.jdbc.DriverConnectionFactory;
 import io.trino.plugin.jdbc.ForBaseJdbc;
 import io.trino.plugin.jdbc.JdbcClient;
 import io.trino.plugin.jdbc.credential.CredentialProvider;
-import io.trino.plugin.jdbc.mapping.RemoteIdentifierSupplier;
 import io.trino.plugin.jdbc.ptf.Query;
 import io.trino.spi.connector.CatalogHandle;
 import io.trino.spi.function.table.ConnectorTableFunction;
@@ -36,7 +35,6 @@ import org.apache.calcite.avatica.remote.GalaxyDruidDriver;
 import java.util.Properties;
 
 import static com.google.inject.multibindings.Multibinder.newSetBinder;
-import static com.google.inject.multibindings.OptionalBinder.newOptionalBinder;
 import static io.trino.plugin.base.galaxy.GalaxySqlSocketFactory.addCatalogId;
 import static io.trino.plugin.base.galaxy.GalaxySqlSocketFactory.addCatalogName;
 import static io.trino.plugin.base.galaxy.GalaxySqlSocketFactory.addTlsEnabled;
@@ -52,11 +50,6 @@ public class DruidJdbcClientModule
     @Override
     public void configure(Binder binder)
     {
-        // Druid driver always return true for storesUpperCaseIdentifiers for any remote objects irrespective of casing, thus returning incorrect results
-        // when case-insensitive-name-matching is set to false (using default implementation DatabaseMetaDataRemoteIdentifierSupplier).
-        // Until the driver is fixed, use the identifier as-is instead of relying on storesUpperCaseIdentifiers.
-        newOptionalBinder(binder, RemoteIdentifierSupplier.class).setBinding().toProvider(() -> (connection, identifier) -> identifier).in(Scopes.SINGLETON);
-
         binder.bind(JdbcClient.class).annotatedWith(ForBaseJdbc.class).to(DruidJdbcClient.class).in(Scopes.SINGLETON);
         newSetBinder(binder, ConnectorTableFunction.class).addBinding().toProvider(Query.class).in(Scopes.SINGLETON);
     }
