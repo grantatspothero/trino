b8baa2e093f796b68aa2b6d271c0f3b4e6437885
Assaf Bern <assaf.bern@starburstdata.com>
2023-07-10 12:38:27 +0300

    Add a test for explain with ChooseAlternativeNode

--- plugin/trino-hive/src/test/java/io/trino/plugin/hive/TestHiveWithPlanAlternativesConnectorTest.java
+++ plugin/trino-hive/src/test/java/io/trino/plugin/hive/TestHiveWithPlanAlternativesConnectorTest.java
@@ -18,6 +18,8 @@ import io.trino.testing.QueryRunner;
 import org.intellij.lang.annotations.Language;
 import org.testng.annotations.Test;
 
+import java.util.regex.Pattern;
+
 import static io.trino.SystemSessionProperties.USE_TABLE_SCAN_NODE_PARTITIONING;
 import static io.trino.testing.TestingNames.randomNameSuffix;
 import static java.util.stream.Collectors.joining;
@@ -59,6 +61,18 @@ public class TestHiveWithPlanAlternativesConnectorTest
         finally {
             assertUpdate("DROP TABLE IF EXISTS test_bucketed_select");
         }
+    }
+
+    @Test
+    public void testExplain()
+    {
+        assertExplain(
+                "EXPLAIN SELECT name FROM nation WHERE nationkey = 8",
+                "ChooseAlternativeNode",
+                "ScanProject",  // filter is subsumed by the connector
+                Pattern.quote("\"nationkey\" = BIGINT '8'"),  // filter is not subsumed by the connector
+                "Estimates: \\{rows: .* \\(.*\\), cpu: .*, memory: .*, network: .*}",
+                "Trino version: .*");
     }
 
     @Override
