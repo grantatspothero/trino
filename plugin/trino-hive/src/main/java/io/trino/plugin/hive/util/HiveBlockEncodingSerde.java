77529e191f11bbd72b972aaf280d205c52555fec
Dain Sundstrom <dain@iq80.com>
2023-07-12 15:46:31 -0700

    Change SqlRow to not be a Block
    (cherry picked from commit a35e233b586f310dfeac89bbab04d41ba0713da3)
    (cherry picked from commit 8b173e2dbaea729c0e9d38473294d0762fa62656)

--- plugin/trino-hive/src/main/java/io/trino/plugin/hive/util/HiveBlockEncodingSerde.java
+++ plugin/trino-hive/src/main/java/io/trino/plugin/hive/util/HiveBlockEncodingSerde.java
@@ -32,7 +32,6 @@ import io.trino.spi.block.MapBlockEncoding;
 import io.trino.spi.block.RowBlockEncoding;
 import io.trino.spi.block.RunLengthBlockEncoding;
 import io.trino.spi.block.ShortArrayBlockEncoding;
-import io.trino.spi.block.SqlRowBlockEncoding;
 import io.trino.spi.block.VariableWidthBlockEncoding;
 import io.trino.spi.type.Type;
 import io.trino.spi.type.TypeId;
@@ -77,7 +76,6 @@ public final class HiveBlockEncodingSerde
         addBlockEncoding(new ArrayBlockEncoding());
         addBlockEncoding(new MapBlockEncoding());
         addBlockEncoding(new RowBlockEncoding());
-        addBlockEncoding(new SqlRowBlockEncoding());
         addBlockEncoding(new RunLengthBlockEncoding());
         addBlockEncoding(new LazyBlockEncoding());
     }
