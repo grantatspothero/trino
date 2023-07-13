5192eb69bd53b589458f963e8f8c337eecbcefe0
Anu Sudarsan <anu.at.infy@gmail.com>
2023-07-13 15:57:02 -0400

    Add retry for 429 error from crdb license url
    Cherry-pick from https://github.com/starburstdata/stargate/pull/10901

--- testing/trino-testing-containers/src/main/java/io/trino/testing/containers/galaxy/ExtendedCockroachContainer.java
+++ testing/trino-testing-containers/src/main/java/io/trino/testing/containers/galaxy/ExtendedCockroachContainer.java
@@ -13,37 +13,19 @@
  */
 package io.trino.testing.containers.galaxy;
 
-import dev.failsafe.Failsafe;
-import dev.failsafe.RetryPolicy;
 import org.jdbi.v3.core.Jdbi;
 import org.testcontainers.containers.CockroachContainer;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
-import java.time.temporal.ChronoUnit;
-
 import static java.lang.String.format;
-import static java.net.HttpURLConnection.HTTP_OK;
-import static java.net.http.HttpClient.newHttpClient;
 
 public final class ExtendedCockroachContainer
         extends CockroachContainer
 {
     private static final String COCKROACH_VERSION = "22.2";
     private static final String COCKROACH_CONTAINER = format("cockroachdb/cockroach:latest-v%s", COCKROACH_VERSION);
-    private static final String COCKROACH_LICENSE_URL = "https://register.cockroachdb.com/api/license?kind=demo&version=v%s&clusterid=%s";
     private static final String REGION = "us-east1";
-
-    private static final HttpClient LICENSE_HTTP_CLIENT = newHttpClient();
-    private static final RetryPolicy<HttpResponse<String>> LICENSE_RETRY_POLICY = RetryPolicy.<HttpResponse<String>>builder()
-            .handle(IOException.class)
-            .withDelay(50, 100, ChronoUnit.MILLIS)
-            .withMaxAttempts(100)
-            .build();
+    private static final String CLUSTER_ORGANIZATION = "Starburst Dev";
+    private static final String DEV_ENTERPRISE_LICENSE_KEY = "crl-0-EICrhrUGGAEiDVN0YXJidXJzdCBEZXY";
 
     public ExtendedCockroachContainer()
     {
@@ -56,24 +38,12 @@ public final class ExtendedCockroachContainer
     {
         super.runInitScriptIfRequired();
         Jdbi.create(() -> createConnection("")).useHandle(handle -> {
-            String clusterId = handle.select("SELECT crdb_internal.cluster_id()").mapTo(String.class).one();
-            handle.execute("SET CLUSTER SETTING \"enterprise.license\" = ?", getCockroachLicense(clusterId));
+            handle.execute("SET CLUSTER SETTING \"cluster.organization\" = ?", CLUSTER_ORGANIZATION);
+            handle.execute("SET CLUSTER SETTING \"enterprise.license\" = ?", DEV_ENTERPRISE_LICENSE_KEY);
             handle.execute("SET CLUSTER SETTING \"kv.closed_timestamp.lead_for_global_reads_override\" = '1ms'");
             handle.execute(format("ALTER DATABASE %s SET PRIMARY REGION \"%s\"", getDatabaseName(), REGION));
             handle.execute("CREATE DATABASE IF NOT EXISTS %s PRIMARY REGION \"%s\" REGIONS = \"%s\" SURVIVE ZONE FAILURE;"
                     .formatted(getDatabaseName() + "_query_history", REGION, REGION));
         });
-    }
-
-    private static String getCockroachLicense(String clusterId)
-    {
-        URI uri = URI.create(format(COCKROACH_LICENSE_URL, COCKROACH_VERSION, clusterId));
-        HttpRequest request = HttpRequest.newBuilder(uri).build();
-        HttpResponse<String> response = Failsafe.with(LICENSE_RETRY_POLICY).get(() ->
-                LICENSE_HTTP_CLIENT.send(request, BodyHandlers.ofString()));
-        if (response.statusCode() != HTTP_OK) {
-            throw new RuntimeException("Invalid Cockroach license response: " + response);
-        }
-        return response.body();
     }
 }
