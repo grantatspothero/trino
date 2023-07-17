bdeb9aeca7162d64fe96cac38b38cb2eede75043
Anu Sudarsan <anu.at.infy@gmail.com>
2023-07-17 11:41:29 -0400

    Disable Kerberos and Impersonation product tests in Hive
    Disable as the feature is not used

--- testing/trino-product-tests-launcher/src/main/java/io/trino/tests/product/launcher/suite/suites/Suite5.java
+++ testing/trino-product-tests-launcher/src/main/java/io/trino/tests/product/launcher/suite/suites/Suite5.java
@@ -15,15 +15,10 @@ package io.trino.tests.product.launcher.suite.suites;
 
 import com.google.common.collect.ImmutableList;
 import io.trino.tests.product.launcher.env.EnvironmentConfig;
-import io.trino.tests.product.launcher.env.environment.EnvSinglenodeHiveImpersonation;
-import io.trino.tests.product.launcher.env.environment.EnvSinglenodeKerberosHiveImpersonation;
-import io.trino.tests.product.launcher.env.environment.EnvSinglenodeKerberosHiveImpersonationWithCredentialCache;
 import io.trino.tests.product.launcher.suite.Suite;
 import io.trino.tests.product.launcher.suite.SuiteTestRun;
 
 import java.util.List;
-
-import static io.trino.tests.product.launcher.suite.SuiteTestRun.testOnEnvironment;
 
 public class Suite5
         extends Suite
@@ -31,16 +26,17 @@ public class Suite5
     @Override
     public List<SuiteTestRun> getTestRuns(EnvironmentConfig config)
     {
-        return ImmutableList.of(
-                testOnEnvironment(EnvSinglenodeHiveImpersonation.class)
-                        .withGroups("configured_features", "storage_formats", "hdfs_impersonation")
-                        .build(),
-                testOnEnvironment(EnvSinglenodeKerberosHiveImpersonation.class)
-                        .withGroups("configured_features", "storage_formats", "hdfs_impersonation", "authorization", "hive_kerberos")
-                        .build(),
-                testOnEnvironment(EnvSinglenodeKerberosHiveImpersonationWithCredentialCache.class)
-                        .withGroups("configured_features", "storage_formats", "hdfs_impersonation", "authorization")
-                        .build());
+        return ImmutableList.of();
+        // Disable Kerberos and impersonation tests in Galaxy-trino, as these features are not used
+//                testOnEnvironment(EnvSinglenodeHiveImpersonation.class)
+//                        .withGroups("configured_features", "storage_formats", "hdfs_impersonation")
+//                        .build(),
+//                testOnEnvironment(EnvSinglenodeKerberosHiveImpersonation.class)
+//                        .withGroups("configured_features", "storage_formats", "hdfs_impersonation", "authorization", "hive_kerberos")
+//                        .build(),
+//                testOnEnvironment(EnvSinglenodeKerberosHiveImpersonationWithCredentialCache.class)
+//                        .withGroups("configured_features", "storage_formats", "hdfs_impersonation", "authorization")
+//                        .build());
         // Rubix caching tests temporarily removed. https://github.com/starburstdata/galaxy-trino/issues/833
                 /*testOnEnvironment(EnvMultinodeHiveCaching.class)
                         .withGroups("configured_features", "hive_caching", "storage_formats")
