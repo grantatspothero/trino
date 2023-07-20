260ca7f64f2a4d8cd6e46169a761f76ac9100d58
Alex Jo <jo.alex2144@gmail.com>
2023-07-20 11:15:02 -0400

    Test Iceberg system tables via the Object Store connector

---
+++ plugin/trino-galaxy-objectstore/src/test/java/io/trino/plugin/objectstore/TestObjectStoreIcebergSystemTables.java
@@ -0,0 +1,65 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.trino.plugin.objectstore;
+
+import io.trino.hdfs.TrinoFileSystemCache;
+import io.trino.plugin.hive.metastore.galaxy.TestingGalaxyMetastore;
+import io.trino.plugin.iceberg.TestIcebergSystemTables;
+import io.trino.server.galaxy.GalaxyCockroachContainer;
+import io.trino.server.security.galaxy.TestingAccountFactory;
+import io.trino.testing.DistributedQueryRunner;
+import org.testng.annotations.BeforeClass;
+
+import static io.trino.plugin.objectstore.ObjectStoreQueryRunner.CATALOG;
+import static io.trino.server.security.galaxy.GalaxyTestHelper.ACCOUNT_ADMIN;
+import static io.trino.server.security.galaxy.TestingAccountFactory.createTestingAccountFactory;
+import static java.lang.String.format;
+
+public class TestObjectStoreIcebergSystemTables
+        extends TestIcebergSystemTables
+{
+    @Override
+    protected DistributedQueryRunner createQueryRunner()
+            throws Exception
+    {
+        closeAfterClass(TrinoFileSystemCache.INSTANCE::closeAll);
+
+        GalaxyCockroachContainer galaxyCockroachContainer = closeAfterClass(new GalaxyCockroachContainer());
+        MinioStorage minio = closeAfterClass(new MinioStorage("test-bucket"));
+        minio.start();
+
+        TestingGalaxyMetastore metastore = closeAfterClass(new TestingGalaxyMetastore(galaxyCockroachContainer));
+
+        TestingLocationSecurityServer locationSecurityServer = closeAfterClass(new TestingLocationSecurityServer((session, location) -> true));
+        TestingAccountFactory testingAccountFactory = closeAfterClass(createTestingAccountFactory(() -> galaxyCockroachContainer));
+
+        return ObjectStoreQueryRunner.builder()
+                .withTableType(TableType.ICEBERG)
+                .withAccountClient(testingAccountFactory.createAccountClient())
+                .withS3Url(minio.getS3Url())
+                .withHiveS3Config(minio.getHiveS3Config())
+                .withMetastore(metastore)
+                .withLocationSecurityServer(locationSecurityServer)
+                .withPlugin(new ObjectStorePlugin())
+                .build();
+    }
+
+    @Override
+    @BeforeClass
+    public void setUp()
+    {
+        super.setUp();
+        getQueryRunner().execute(format("GRANT SELECT ON \"" + CATALOG + "\".\"test_schema\".\"*\" TO ROLE %s WITH GRANT OPTION", ACCOUNT_ADMIN));
+    }
+}
