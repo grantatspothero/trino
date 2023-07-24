4f00c1aa23942e7495c3039aca208071b0cc1520
David Phillips <david@acz.org>
2023-07-24 15:22:47 -0700

    Remove unused Runnable doAs method for HDFS authentication
    (cherry picked from commit 2b35ff06d4dc8e1ecec46dcacea5f0d1050315ee)

--- lib/trino-hdfs/src/main/java/io/trino/hdfs/HdfsEnvironment.java
+++ lib/trino-hdfs/src/main/java/io/trino/hdfs/HdfsEnvironment.java
@@ -124,11 +124,6 @@ public class HdfsEnvironment
         return hdfsAuthentication.doAs(identity, action);
     }
 
-    public void doAs(ConnectorIdentity identity, Runnable action)
-    {
-        hdfsAuthentication.doAs(identity, action);
-    }
-
     private static void stopFileSystemStatsThread()
     {
         try {
