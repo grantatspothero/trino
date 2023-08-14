86c6944c7db1b388c4deb19ce795c22a49956f26
Piotr Findeisen <piotr.findeisen@gmail.com>
2023-08-14 11:05:20 +0200

    👷 Rename Starburst Snowflake product test env
    Prep for ingesting Snowflake connector from OSS.
    
    (cherry picked from commit 0d7d8469fb08d53c931664d085897bd438b68466)

--- testing/trino-product-tests-groups/src/main/java/io/trino/tests/product/TestGroups.java
+++ testing/trino-product-tests-groups/src/main/java/io/trino/tests/product/TestGroups.java
@@ -62,9 +62,9 @@ public final class TestGroups
     public static final String AUTHORIZATION = "authorization";
     public static final String AZURE = "azure";
     public static final String CASSANDRA = "cassandra";
-    public static final String SNOWFLAKE = "snowflake";
     public static final String POSTGRESQL = "postgresql";
     public static final String SQLSERVER = "sqlserver";
+    public static final String STARBURST_SNOWFLAKE = "starburst_snowflake";
     public static final String SYNAPSE = "synapse";
     public static final String SAP_HANA = "sap-hana";
     public static final String LDAP = "ldap";
