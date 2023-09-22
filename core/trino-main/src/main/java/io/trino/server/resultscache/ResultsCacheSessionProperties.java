de5918043134b332b4ff05c5638bb444d5ba302a
mduggan-starburst <matt.duggan@starburstdata.com>
2023-09-22 14:42:33 -0400

    Add two new results cache session properties
    Two new session properties have been defined in Trino to allow for
    them to be set using SET SESSION.  These two new properties:
    'skip_results_cache' and 'results_cache_reuse_period' are consumed
    by the dispatcher and not used by Trino.  They are filtered out by the
    dispatcher as well, prior to the request being proxied to Trino.

--- core/trino-main/src/main/java/io/trino/server/resultscache/ResultsCacheSessionProperties.java
+++ core/trino-main/src/main/java/io/trino/server/resultscache/ResultsCacheSessionProperties.java
@@ -22,6 +22,8 @@ import io.trino.spi.session.PropertyMetadata;
 import java.util.List;
 import java.util.Optional;
 
+import static io.trino.plugin.base.session.PropertyMetadataUtil.durationProperty;
+import static io.trino.spi.session.PropertyMetadata.booleanProperty;
 import static io.trino.spi.session.PropertyMetadata.longProperty;
 import static io.trino.spi.session.PropertyMetadata.stringProperty;
 
@@ -30,6 +32,10 @@ public final class ResultsCacheSessionProperties
 {
     private static final String CACHE_KEY = "galaxy_results_cache_key";
     private static final String CACHE_ENTRY_MAX_SIZE_BYTES = "galaxy_results_cache_entry_max_size_bytes";
+
+    // These properties are consumed by the dispatcher and not used within Trino
+    private static final String SKIP_RESULTS_CACHE_SESSION_PROPERTY = "skip_results_cache";
+    private static final String RESULTS_CACHE_VISIBILITY_SESSION_PROPERTY = "results_cache_reuse_period";
 
     private static final List<PropertyMetadata<?>> sessionProperties = ImmutableList.of(
             stringProperty(
@@ -41,7 +47,17 @@ public final class ResultsCacheSessionProperties
                     CACHE_ENTRY_MAX_SIZE_BYTES,
                     "Maximum size for a cache entry in bytes",
                     null,
-                    true));
+                    true),
+            booleanProperty(
+                    SKIP_RESULTS_CACHE_SESSION_PROPERTY,
+                    "Skip using the results cache regardless of if cached results are available",
+                    null,
+                    false),
+            durationProperty(
+                    RESULTS_CACHE_VISIBILITY_SESSION_PROPERTY,
+                    "Duration of time where that allows reuse of cached results for a query",
+                    null,
+                    false));
 
     @Override
     public List<PropertyMetadata<?>> getSessionProperties()
