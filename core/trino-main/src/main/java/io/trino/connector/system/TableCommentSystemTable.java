da88a1dd2147956d33194e485e965a9d9987345b
Piotr Findeisen <piotr.findeisen@gmail.com>
2023-10-04 11:14:18 +0200

    Optimize table_comments with schema IN predicate
    Before the changes, `system.metadata.table_comments` queried with
    `schema_name IN ...` predicate would first read all comments from all
    tables within catalog, and only then filter by schema. This commit
    introduces filtering by schema first. For now this is done on the engine
    side, but with something like generified `Constraint` class we could
    give the connector the opportunity to process the information even
    better.
    
    (cherry picked from commit 2d163d5702e7e9ed941ff2d0e4c0ad374173e657)

--- core/trino-main/src/main/java/io/trino/connector/system/TableCommentSystemTable.java
+++ core/trino-main/src/main/java/io/trino/connector/system/TableCommentSystemTable.java
@@ -16,6 +16,7 @@ package io.trino.connector.system;
 import com.google.common.collect.ImmutableSet;
 import com.google.inject.Inject;
 import io.airlift.log.Logger;
+import io.airlift.slice.Slice;
 import io.trino.FullConnectorSession;
 import io.trino.Session;
 import io.trino.metadata.MaterializedViewDefinition;
@@ -100,15 +101,25 @@ public class TableCommentSystemTable
             return table.build().cursor();
         }
 
-        Optional<String> schemaFilter = tryGetSingleVarcharValue(schemaDomain);
         Optional<String> tableFilter = tryGetSingleVarcharValue(tableDomain);
 
         Session session = ((FullConnectorSession) connectorSession).getSession();
 
         for (String catalog : listCatalogNames(session, metadata, accessControl, catalogDomain)) {
-            QualifiedTablePrefix prefix = tablePrefix(catalog, schemaFilter, tableFilter);
-
-            addTableCommentForCatalog(session, prefix, catalog, table);
+            // TODO A connector may be able to pull information from multiple schemas at once, so pass the schema filter to the connector instead.
+            // TODO Support LIKE predicates on schema name (or any other functional predicates), so pass the schema filter as Constraint-like to the connector.
+            if (schemaDomain.isNullableDiscreteSet()) {
+                for (Object slice : schemaDomain.getNullableDiscreteSet().getNonNullValues()) {
+                    String schemaName = ((Slice) slice).toStringUtf8();
+                    if (isImpossibleObjectName(schemaName)) {
+                        continue;
+                    }
+                    addTableCommentForCatalog(session, tablePrefix(catalog, Optional.of(schemaName), tableFilter), catalog, table);
+                }
+            }
+            else {
+                addTableCommentForCatalog(session, tablePrefix(catalog, Optional.empty(), tableFilter), catalog, table);
+            }
         }
 
         return table.build().cursor();
