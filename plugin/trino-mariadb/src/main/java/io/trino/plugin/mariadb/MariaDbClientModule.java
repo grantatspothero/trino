8094300d774a9f7d9c845c3602bad4c68e96695f
Piotr Findeisen <piotr.findeisen@gmail.com>
2023-10-12 17:30:35 +0200

    Add MariaDB statistics support
    Get table statistics for MariaDB tables.

--- plugin/trino-mariadb/src/main/java/io/trino/plugin/mariadb/MariaDbClientModule.java
+++ plugin/trino-mariadb/src/main/java/io/trino/plugin/mariadb/MariaDbClientModule.java
@@ -30,6 +30,7 @@ import io.trino.plugin.jdbc.DriverConnectionFactory;
 import io.trino.plugin.jdbc.ForBaseJdbc;
 import io.trino.plugin.jdbc.JdbcClient;
 import io.trino.plugin.jdbc.JdbcMetadataConfig;
+import io.trino.plugin.jdbc.JdbcStatisticsConfig;
 import io.trino.plugin.jdbc.credential.CredentialProvider;
 import io.trino.plugin.jdbc.ptf.Query;
 import io.trino.plugin.mariadb.galaxy.GalaxyMariaDbSocketFactory;
@@ -54,6 +55,7 @@ public class MariaDbClientModule
         binder.bind(JdbcClient.class).annotatedWith(ForBaseJdbc.class).to(MariaDbClient.class).in(Scopes.SINGLETON);
         configBinder(binder).bindConfigDefaults(JdbcMetadataConfig.class, config -> config.setListColumnsMode(JdbcMetadataConfig.ListColumnsMode.DMA));
         configBinder(binder).bindConfig(MariaDbJdbcConfig.class);
+        configBinder(binder).bindConfig(JdbcStatisticsConfig.class);
         binder.install(new DecimalModule());
         newSetBinder(binder, ConnectorTableFunction.class).addBinding().toProvider(Query.class).in(Scopes.SINGLETON);
     }
