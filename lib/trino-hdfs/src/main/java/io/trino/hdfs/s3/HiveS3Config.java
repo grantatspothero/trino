74fff5e2b12cceaf0e2461335e8137aeb7fbc1d3
Mateusz "Serafin" Gajewski <mateusz.gajewski@gmail.com>
2023-10-24 20:59:36 +0200

    Add @ConfigSecuritySensitive to Hive S3 proxy password
    (cherry picked from commit 00480a7f757f096b92808bedfe130f61b82292f2)
    (cherry picked from commit 64a6cc2a13578097b73957b7a224325144a05aac)

--- lib/trino-hdfs/src/main/java/io/trino/hdfs/s3/HiveS3Config.java
+++ lib/trino-hdfs/src/main/java/io/trino/hdfs/s3/HiveS3Config.java
@@ -614,6 +614,7 @@ public class HiveS3Config
     }
 
     @Config("hive.s3.proxy.password")
+    @ConfigSecuritySensitive
     public HiveS3Config setS3ProxyPassword(String s3proxyPassword)
     {
         this.s3proxyPassword = s3proxyPassword;
