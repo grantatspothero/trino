be7f430784035e07697a4349f25177302bcbda13
Karol Sobczak <napewnotrafi@gmail.com>
2023-11-17 13:59:18 +0100

    Simplify NodeRepresentation#getEstimates
    (cherry picked from commit b99ef18dd49c6dcf7412fd50347db93d2e18f894)

--- testing/trino-tests/src/test/java/io/trino/execution/TestEventListenerBasic.java
+++ testing/trino-tests/src/test/java/io/trino/execution/TestEventListenerBasic.java
@@ -1310,6 +1310,7 @@ public class TestEventListenerBasic
                                 "inputPreSortedBy", "[]"),
                         ImmutableList.of(typedSymbol("symbol_1", DOUBLE)),
                         ImmutableList.of(),
+                        ImmutableList.of(),
                         ImmutableList.of(new PlanNodeStatsAndCostSummary(10., 90., 90., 0., 0.)),
                         ImmutableList.of(new JsonRenderedNode(
                                 "0",
