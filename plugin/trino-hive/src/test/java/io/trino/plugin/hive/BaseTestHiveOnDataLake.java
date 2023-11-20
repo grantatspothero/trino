1a05bc29effb42bbb28e0324b154be7b84afc18c
Karol Sobczak <napewnotrafi@gmail.com>
2023-11-20 18:33:00 +0100

    Remove try catch around DeterminePartitionCount stats estimation
    It's no longer needed as stats rules have been improved

--- plugin/trino-hive/src/test/java/io/trino/plugin/hive/BaseTestHiveOnDataLake.java
+++ plugin/trino-hive/src/test/java/io/trino/plugin/hive/BaseTestHiveOnDataLake.java
@@ -1624,7 +1624,7 @@ public abstract class BaseTestHiveOnDataLake
                             (null, null, null, null, 5.0, null, null)
                         """);
         // TODO (https://github.com/trinodb/trino/issues/15998) fix selective ANALYZE for table with non-canonical partition values
-        assertQueryFails("ANALYZE " + getFullyQualifiedTestTableName(externalTableName) + " WITH (partitions = ARRAY[ARRAY['4']])", "Partition no longer exists: month=4");
+        assertQueryFails("ANALYZE " + getFullyQualifiedTestTableName(externalTableName) + " WITH (partitions = ARRAY[ARRAY['4']])", ".*Partition.*not found.*");
 
         assertUpdate("DROP TABLE " + getFullyQualifiedTestTableName(externalTableName));
         assertUpdate("DROP TABLE " + getFullyQualifiedTestTableName(tableName));
