e30ab991a14c3b707262cad0a341a774ddafca36
Grant Nicholas <grant.nicholas@starburstdata.com>
2023-11-27 16:43:11 -0600

    Make S3FileSystemFactory use configured AWS access keys for STS calls

--- lib/trino-filesystem-s3/src/main/java/io/trino/filesystem/s3/S3FileSystemFactory.java
+++ lib/trino-filesystem-s3/src/main/java/io/trino/filesystem/s3/S3FileSystemFactory.java
@@ -53,10 +53,8 @@ public final class S3FileSystemFactory
     {
         S3ClientBuilder s3 = S3Client.builder();
 
-        if ((config.getAwsAccessKey() != null) && (config.getAwsSecretKey() != null)) {
-            s3.credentialsProvider(StaticCredentialsProvider.create(
-                    AwsBasicCredentials.create(config.getAwsAccessKey(), config.getAwsSecretKey())));
-        }
+        Optional<StaticCredentialsProvider> staticCredentialsProvider = getStaticCredentialsProvider(config);
+        staticCredentialsProvider.ifPresent(s3::credentialsProvider);
 
         Optional.ofNullable(config.getRegion()).map(Region::of).ifPresent(s3::region);
         Optional.ofNullable(config.getEndpoint()).map(URI::create).ifPresent(s3::endpointOverride);
@@ -68,6 +66,7 @@ public final class S3FileSystemFactory
             Optional.ofNullable(config.getStsRegion())
                     .or(() -> Optional.ofNullable(config.getRegion()))
                     .map(Region::of).ifPresent(sts::region);
+            staticCredentialsProvider.ifPresent(sts::credentialsProvider);
 
             s3.credentialsProvider(StsAssumeRoleCredentialsProvider.builder()
                     .refreshRequest(request -> request
@@ -120,5 +119,14 @@ public final class S3FileSystemFactory
     public TrinoFileSystem create(ConnectorIdentity identity)
     {
         return new S3FileSystem(client, context, supportLegacyCorruptedPaths);
+    }
+
+    private static Optional<StaticCredentialsProvider> getStaticCredentialsProvider(S3FileSystemConfig config)
+    {
+        if ((config.getAwsAccessKey() != null) || (config.getAwsSecretKey() != null)) {
+            return Optional.of(StaticCredentialsProvider.create(
+                    AwsBasicCredentials.create(config.getAwsAccessKey(), config.getAwsSecretKey())));
+        }
+        return Optional.empty();
     }
 }
