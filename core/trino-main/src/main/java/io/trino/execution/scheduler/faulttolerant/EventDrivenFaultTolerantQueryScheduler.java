9b431128597d84a1c763c676a0d9a071631e79cd
Dejan Mircevski <dejan.mircevski@starburstdata.com>
2024-01-23 15:32:00 -0500

    Mark new tasks readyForScheduling when fragment children are finished
    Previously, StageExecution#updatePartition() always created new tasks as speculative.  But if the
    fragment's children are completed, the task is not really speculative -- it can get scheduled without fear
    of its precursors failing.
    
    (cherry picked from commit a6090d3e61b00377b5a4f995ac71acda6eb5f02b)

--- core/trino-main/src/main/java/io/trino/execution/scheduler/faulttolerant/EventDrivenFaultTolerantQueryScheduler.java
+++ core/trino-main/src/main/java/io/trino/execution/scheduler/faulttolerant/EventDrivenFaultTolerantQueryScheduler.java
@@ -1327,6 +1327,11 @@ public class EventDrivenFaultTolerantQueryScheduler
                 return IsReadyForExecutionResult.notReady();
             }
 
+            StageExecution stageExecution = stageExecutions.get(getStageId(subPlan.getFragment().getId()));
+            if (stageExecution != null) {
+                stageExecution.setSpeculative(speculative);
+            }
+
             if (speculative) {
                 log.debug("scheduling speculative %s/%s; sources: finished=%s; kinds=%s",
                         queryStateMachine.getQueryId(),
@@ -1895,6 +1900,8 @@ public class EventDrivenFaultTolerantQueryScheduler
         private final int maxTaskExecutionAttempts;
         private final int schedulingPriority;
         private final boolean eager;
+        private boolean speculative;
+
         private final DynamicFilterService dynamicFilterService;
         private final long[] outputDataSize;
 
@@ -2029,6 +2036,11 @@ public class EventDrivenFaultTolerantQueryScheduler
             return exchangeClosed;
         }
 
+        public void setSpeculative(boolean speculative)
+        {
+            this.speculative = speculative;
+        }
+
         public void addPartition(int partitionId, NodeRequirements nodeRequirements)
         {
             if (getState().isDone()) {
@@ -2065,7 +2077,10 @@ public class EventDrivenFaultTolerantQueryScheduler
             partition.addSplits(planNodeId, splits, noMoreSplits);
             if (readyForScheduling && !partition.isTaskScheduled()) {
                 partition.setTaskScheduled(true);
-                return Optional.of(PrioritizedScheduledTask.createSpeculative(stage.getStageId(), taskPartitionId, schedulingPriority, eager));
+                PrioritizedScheduledTask task = speculative ?
+                        PrioritizedScheduledTask.createSpeculative(stage.getStageId(), taskPartitionId, schedulingPriority, eager) :
+                        PrioritizedScheduledTask.create(stage.getStageId(), taskPartitionId, schedulingPriority);
+                return Optional.of(task);
             }
             return Optional.empty();
         }
