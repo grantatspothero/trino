567e62bfac947455c6495aec2710434212f2454d
Jack Klamer <jack.klamer@starburstdata.com>
2024-02-09 11:23:30 -0600

    Update for scheduler http client timeouts
    (cherry picked from commit 941abe8ef5bce6a94659fce5340dfebff778b046)
    (cherry picked from commit 875f64147e5d1607592eb332413a24874dcba1d2)

--- core/trino-main/src/main/java/io/trino/server/CoordinatorModule.java
+++ core/trino-main/src/main/java/io/trino/server/CoordinatorModule.java
@@ -350,8 +350,8 @@ public class CoordinatorModule
                 .withTracing()
                 .withFilter(GenerateTraceTokenRequestFilter.class)
                 .withConfigDefaults(config -> {
-                    config.setIdleTimeout(new Duration(30, SECONDS));
-                    config.setRequestTimeout(new Duration(15, SECONDS));
+                    config.setIdleTimeout(new Duration(60, SECONDS));
+                    config.setRequestTimeout(new Duration(20, SECONDS));
                     config.setMaxConnectionsPerServer(250);
                 }).build());
 
