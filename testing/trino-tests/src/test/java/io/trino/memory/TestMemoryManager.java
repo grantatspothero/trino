f13fe131399fb11eec459b1732b653872c185ad3
Piotr Findeisen <piotr.findeisen@gmail.com>
2024-02-21 15:05:55 +0100

    Enable TestMemoryManager.testClusterPools test
    It was disabled in commit e2b81fa07a2f3fa5e4bbb857e16a53b53633c96b.

--- testing/trino-tests/src/test/java/io/trino/memory/TestMemoryManager.java
+++ testing/trino-tests/src/test/java/io/trino/memory/TestMemoryManager.java
@@ -28,7 +28,6 @@ import io.trino.tests.tpch.TpchQueryRunnerBuilder;
 import org.intellij.lang.annotations.Language;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.Timeout;
@@ -223,7 +222,6 @@ public class TestMemoryManager
 
     @Test
     @Timeout(240)
-    @Disabled
     public void testClusterPools()
             throws Exception
     {
