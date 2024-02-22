a42f821f66f08ab842256592ffeaf829a3c4f77b
Elon Azoulay <elon.azoulay@gmail.com>
2024-02-21 22:50:41 -0800

    Add application default credentials to GcsStorageFactory
    This enables the use of workload identity federation.
    
    (cherry picked from commit b90fe09f4f28ee1f2cc6a74283b311972242d673)
    (cherry picked from commit 5bd6cec842270cb9f8784b54ce6b1d327226dc6b)

--- lib/trino-filesystem-gcs/src/main/java/io/trino/filesystem/gcs/GcsStorageFactory.java
+++ lib/trino-filesystem-gcs/src/main/java/io/trino/filesystem/gcs/GcsStorageFactory.java
@@ -17,7 +17,6 @@ import com.google.api.gax.retrying.RetrySettings;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
-import com.google.common.base.VerifyException;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.google.inject.Inject;
@@ -101,7 +100,14 @@ public class GcsStorageFactory
                 }
             }
             else {
-                credentials = jsonGoogleCredential.orElseThrow(() -> new VerifyException("GCS credentials not configured"));
+                credentials = jsonGoogleCredential.orElseGet(() -> {
+                    try {
+                        return GoogleCredentials.getApplicationDefault();
+                    }
+                    catch (IOException e) {
+                        throw new UncheckedIOException(e);
+                    }
+                });
             }
             StorageOptions.Builder storageOptionsBuilder = StorageOptions.newBuilder();
             if (projectId != null) {
