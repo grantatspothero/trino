349312ded86205497497cbdab120cf68d4de76e0
Piotr Findeisen <piotr.findeisen@gmail.com>
2024-03-22 12:27:57 +0100

    Fix spurious FTE failure in partitioned queries
    (cherry picked from commit eab80d5d182ddc7afae336f13f8ba4c1b2ce7643)

--- core/trino-main/src/main/java/io/trino/execution/scheduler/faulttolerant/EventDrivenFaultTolerantQueryScheduler.java
+++ core/trino-main/src/main/java/io/trino/execution/scheduler/faulttolerant/EventDrivenFaultTolerantQueryScheduler.java
@@ -2138,7 +2138,7 @@ public class EventDrivenFaultTolerantQueryScheduler
         public void noMorePartitions()
         {
             noMorePartitions = true;
-            nextPartitionId = partitions.keySet().intStream().max().orElseThrow() + 1;
+            nextPartitionId = partitions.keySet().intStream().max().orElse(0) + 1;
             if (getState().isDone()) {
                 return;
             }
