2aa98d341063ce167e3d2e656712de76b0ba8bad
Charles Morgan <charles.morgan@starburstdata.com>
2024-03-26 11:24:09 -0700

    Fix flaky azure network tracking test

--- lib/trino-filesystem-azure/src/test/java/io/trino/filesystem/azure/TestAzureFileSystemNetworkTracking.java
+++ lib/trino-filesystem-azure/src/test/java/io/trino/filesystem/azure/TestAzureFileSystemNetworkTracking.java
@@ -40,6 +40,7 @@ import io.trino.spi.TrinoException;
 import io.trino.spi.connector.CatalogHandle;
 import io.trino.spi.galaxy.CatalogNetworkMonitor;
 import io.trino.spi.security.ConnectorIdentity;
+import org.assertj.core.api.InstanceOfAssertFactories;
 import org.assertj.core.data.Percentage;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
@@ -51,6 +52,7 @@ import org.junit.jupiter.api.TestInstance.Lifecycle;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.UUID;
+import java.util.concurrent.ThreadLocalRandom;
 
 import static com.azure.storage.blob.models.AccountKind.BLOB_STORAGE;
 import static com.azure.storage.blob.models.AccountKind.STORAGE_V2;
@@ -59,10 +61,10 @@ import static com.google.common.base.Preconditions.checkState;
 import static io.trino.filesystem.azure.AbstractTestAzureFileSystem.AccountKind.BLOB;
 import static io.trino.filesystem.azure.AbstractTestAzureFileSystem.AccountKind.FLAT;
 import static io.trino.filesystem.azure.AbstractTestAzureFileSystem.AccountKind.HIERARCHICAL;
-import static io.trino.spi.StandardErrorCode.GENERIC_INSUFFICIENT_RESOURCES;
 import static java.util.Locale.ROOT;
 import static java.util.Objects.requireNonNull;
 import static java.util.UUID.randomUUID;
+import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -70,29 +72,26 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 public class TestAzureFileSystemNetworkTracking
 {
     private static final int BYTES_IN_MEGABYTE = 1024 * 1024;
-    private static final String CATALOG_NAME = "catalog";
-    private static final String CATALOG_ID = "%s:normal:1".formatted(CATALOG_NAME);
-    private static final DataSize CROSS_REGION_READ_LIMIT = DataSize.of(17, Unit.MEGABYTE);
-    private static final DataSize CROSS_REGION_WRITE_LIMIT = DataSize.of(17, Unit.MEGABYTE);
+    private static final DataSize HIGH_CROSS_REGION_LIMIT = DataSize.of(1, Unit.GIGABYTE);
+    private static final DataSize ZERO_CROSS_REGION_LIMIT = DataSize.ofBytes(0);
+    private static final ConnectorIdentity TESTING_CONNECTOR_IDENTITY = ConnectorIdentity.ofUser("test");
 
-    private String account;
-    private StorageSharedKeyCredential credential;
-    private AccountKind accountKind;
-    private String containerName;
-    private Location rootLocation;
-    private BlobContainerClient blobContainerClient;
-    private TrinoFileSystem fileSystem;
+    private static final String STORAGE_ACCOUNT = getRequiredEnvironmentVariable("ABFS_ACCOUNT");
+    private static final String ACCOUNT_KEY = getRequiredEnvironmentVariable("ABFS_ACCESS_KEY");
+    private static StorageSharedKeyCredential credential;
+    private static AccountKind accountKind;
+    private static String containerName;
+    private static Location rootLocation;
+    private static BlobContainerClient blobContainerClient;
 
     @BeforeAll
-    void setup()
+    static void setup()
             throws IOException
     {
-        this.account = getRequiredEnvironmentVariable("ABFS_ACCOUNT");
-        String accountKey = getRequiredEnvironmentVariable("ABFS_ACCESS_KEY");
         AccountKind expectedAccountKind = FLAT;
-        credential = new StorageSharedKeyCredential(account, accountKey);
+        credential = new StorageSharedKeyCredential(STORAGE_ACCOUNT, ACCOUNT_KEY);
 
-        String blobEndpoint = "https://%s.blob.core.windows.net".formatted(account);
+        String blobEndpoint = "https://%s.blob.core.windows.net".formatted(STORAGE_ACCOUNT);
         BlobServiceClient blobServiceClient = new BlobServiceClientBuilder()
                 .endpoint(blobEndpoint)
                 .credential(credential)
@@ -101,33 +100,19 @@ public class TestAzureFileSystemNetworkTracking
         checkState(accountKind == expectedAccountKind, "Expected %s account, but found %s".formatted(expectedAccountKind, accountKind));
 
         containerName = "test-%s-%s".formatted(accountKind.name().toLowerCase(ROOT), randomUUID());
-        rootLocation = Location.of("abfs://%s@%s.dfs.core.windows.net/".formatted(containerName, account));
+        rootLocation = Location.of("abfs://%s@%s.dfs.core.windows.net/".formatted(containerName, STORAGE_ACCOUNT));
 
         blobContainerClient = blobServiceClient.getBlobContainerClient(containerName);
         // this will fail if the container already exists, which is what we want
         blobContainerClient.create();
 
-        fileSystem = new AzureFileSystemFactory(
-                OpenTelemetry.noop(),
-                new AzureAuthAccessKey(accountKey),
-                new AzureFileSystemConfig(),
-                CatalogHandle.fromId(CATALOG_ID),
-                new LocalRegionConfig()
-                        .setAllowedIpAddresses(ImmutableList.of("0.0.0.0")),
-                new CrossRegionConfig()
-                        .setAllowCrossRegionAccess(true)
-                        .setCrossRegionReadLimit(CROSS_REGION_READ_LIMIT)
-                        .setCrossRegionWriteLimit(CROSS_REGION_WRITE_LIMIT))
-                .create(ConnectorIdentity.ofUser("test"));
-
         cleanupFiles();
     }
 
     @AfterAll
-    void tearDown()
+    static void tearDown()
     {
         credential = null;
-        fileSystem = null;
         if (blobContainerClient != null) {
             blobContainerClient.deleteIfExists();
             blobContainerClient = null;
@@ -156,11 +141,11 @@ public class TestAzureFileSystemNetworkTracking
         throw new IOException("Unsupported account kind '%s'".formatted(accountInfo.getAccountKind()));
     }
 
-    private void cleanupFiles()
+    private static void cleanupFiles()
     {
         if (accountKind == HIERARCHICAL) {
             DataLakeFileSystemClient fileSystemClient = new DataLakeFileSystemClientBuilder()
-                    .endpoint("https://%s.dfs.core.windows.net".formatted(account))
+                    .endpoint("https://%s.dfs.core.windows.net".formatted(STORAGE_ACCOUNT))
                     .fileSystemName(containerName)
                     .credential(credential)
                     .buildClient();
@@ -184,6 +169,11 @@ public class TestAzureFileSystemNetworkTracking
     public void testAzureFileSystemNetworkTracking()
             throws IOException
     {
+        String catalogName = newCatalogName();
+        String catalogId = createCatalogId(catalogName);
+        AzureFileSystemFactory fileSystemFactory = createAzureFileSystemFactory(catalogId, HIGH_CROSS_REGION_LIMIT, HIGH_CROSS_REGION_LIMIT);
+        TrinoFileSystem fileSystem = fileSystemFactory.create(TESTING_CONNECTOR_IDENTITY);
+
         Location tempBlobLocation = createLocation("networkTracking/%s".formatted(UUID.randomUUID()));
         // write a 16 MB file
         TrinoOutputFile outputFile = fileSystem.newOutputFile(tempBlobLocation);
@@ -197,7 +187,7 @@ public class TestAzureFileSystemNetworkTracking
         }
 
         // check that network monitor accurately tracked the bytes written with minimal difference
-        CatalogNetworkMonitor catalogNetworkMonitor = CatalogNetworkMonitor.getCrossRegionCatalogNetworkMonitor(CATALOG_NAME, CATALOG_ID, CROSS_REGION_READ_LIMIT.toBytes(), CROSS_REGION_WRITE_LIMIT.toBytes());
+        CatalogNetworkMonitor catalogNetworkMonitor = CatalogNetworkMonitor.getCrossRegionCatalogNetworkMonitor(catalogName, catalogId, HIGH_CROSS_REGION_LIMIT.toBytes(), HIGH_CROSS_REGION_LIMIT.toBytes());
         assertThat(catalogNetworkMonitor.getCrossRegionWriteBytes()).isCloseTo(16 * BYTES_IN_MEGABYTE, Percentage.withPercentage(0.1));
 
         // read the 16 MB file in
@@ -219,10 +209,66 @@ public class TestAzureFileSystemNetworkTracking
         assertThat(catalogNetworkMonitor.getCrossRegionReadBytes()).isCloseTo(16 * BYTES_IN_MEGABYTE, Percentage.withPercentage(0.1));
 
         fileSystem.deleteFile(tempBlobLocation);
+    }
+
+    @Test
+    public void testCrossRegionLimits()
+    {
         // check that cross-region limit works
-        OutputStream outputStream = outputFile.create();
-        outputStream.write(new byte[BYTES_IN_MEGABYTE]);
-        assertThatThrownBy(outputStream::close).hasRootCause(new TrinoException(GENERIC_INSUFFICIENT_RESOURCES, "Cross-region write data transfer limit of 0GB per worker exceeded. To increase this limit, contact Starburst support."));
+        String catalogName = newCatalogName();
+        String catalogId = createCatalogId(catalogName);
+        AzureFileSystemFactory fileSystemFactory = createAzureFileSystemFactory(catalogId, ZERO_CROSS_REGION_LIMIT, ZERO_CROSS_REGION_LIMIT);
+        TrinoFileSystem fileSystem = fileSystemFactory.create(TESTING_CONNECTOR_IDENTITY);
+        Location tempBlobLocation = createLocation("networkTracking/%s".formatted(UUID.randomUUID()));
+
+        TrinoOutputFile outputFile = fileSystem.newOutputFile(tempBlobLocation);
+        assertThatThrownBy(() -> {
+            try (OutputStream outputStream = outputFile.create()) {
+                outputStream.write(new byte[BYTES_IN_MEGABYTE]);
+            }
+        }).satisfies(throwable -> {
+            if (throwable instanceof TrinoException) {
+                assertThat(throwable)
+                        .extracting(Throwable::getMessage, as(InstanceOfAssertFactories.STRING))
+                        .containsAnyOf(
+                                "Cross-region read/write data transfer limits of 0GB / 0GB per worker exceeded. To increase these limits, contact Starburst support.",
+                                "Cross-region write data transfer limit of 0GB per worker exceeded. To increase this limit, contact Starburst support.");
+            }
+            else {
+                assertThat(throwable)
+                        .rootCause()
+                        .isInstanceOf(TrinoException.class)
+                        .extracting(Throwable::getMessage, as(InstanceOfAssertFactories.STRING))
+                        .containsAnyOf(
+                                "Cross-region read/write data transfer limits of 0GB / 0GB per worker exceeded. To increase these limits, contact Starburst support.",
+                                "Cross-region write data transfer limit of 0GB per worker exceeded. To increase this limit, contact Starburst support.");
+            }
+        });
+    }
+
+    private String newCatalogName()
+    {
+        return "cross-region-catalog-%s".formatted(ThreadLocalRandom.current().nextLong(0, Long.MAX_VALUE));
+    }
+
+    private String createCatalogId(String catalogName)
+    {
+        return "%s:normal:1".formatted(catalogName);
+    }
+
+    private AzureFileSystemFactory createAzureFileSystemFactory(String catalogId, DataSize crossRegionReadLimit, DataSize crossRegionWriteLimit)
+    {
+        return new AzureFileSystemFactory(
+                OpenTelemetry.noop(),
+                new AzureAuthAccessKey(ACCOUNT_KEY),
+                new AzureFileSystemConfig(),
+                CatalogHandle.fromId(catalogId),
+                new LocalRegionConfig()
+                        .setAllowedIpAddresses(ImmutableList.of("0.0.0.0")),
+                new CrossRegionConfig()
+                        .setAllowCrossRegionAccess(true)
+                        .setCrossRegionReadLimit(crossRegionReadLimit)
+                        .setCrossRegionWriteLimit(crossRegionWriteLimit));
     }
 
     private Location createLocation(String path)
